096ba75808b855b54c1141a5331ec80a6f323587
Saver12 <saver12@yandex.ru>
2016-04-03 18:12:22 +0300

    Divided implementations into Spring profiles: jdbc, jpa, datajpa

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -28,7 +28,7 @@ import static ru.javawebinar.topjava.UserTestData.*;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(Profiles.HSQLDB)
+@ActiveProfiles({Profiles.POSTGRES, Profiles.DATAJPA})
 public class UserServiceTest {
 
     @Autowired
